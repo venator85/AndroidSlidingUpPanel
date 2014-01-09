db451de5d9012f75387afafbac803c5a2ceed80a
Alessio Bianchi <venator85@gmail.com>
2014-01-09 22:38:48 +0100

    project, better sample

--- demo/src/com/sothree/slidinguppanel/demo/DemoActivity.java
+++ demo/src/com/sothree/slidinguppanel/demo/DemoActivity.java
@@ -1,103 +1,89 @@
 package com.sothree.slidinguppanel.demo;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
 import com.sothree.slidinguppanel.SlidingUpPanelLayout;
 import com.sothree.slidinguppanel.SlidingUpPanelLayout.PanelSlideListener;
 
-import android.net.Uri;
-import android.os.Bundle;
-import android.app.Activity;
-import android.content.Intent;
-import android.text.Html;
-import android.text.method.LinkMovementMethod;
-import android.util.Log;
-import android.view.Menu;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.Window;
-import android.widget.TextView;
-
 public class DemoActivity extends Activity {
-    private static final String TAG = "DemoActivity";
+	private static final String TAG = "DemoActivity";
 
-    public static final String SAVED_STATE_ACTION_BAR_HIDDEN = "saved_state_action_bar_hidden";
+	public static final String SAVED_STATE_ACTION_BAR_HIDDEN = "saved_state_action_bar_hidden";
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        getWindow().requestFeature(Window.FEATURE_ACTION_BAR_OVERLAY);
-        setContentView(R.layout.activity_demo);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_demo);
 
-        SlidingUpPanelLayout layout = (SlidingUpPanelLayout) findViewById(R.id.sliding_layout);
-        layout.setShadowDrawable(getResources().getDrawable(R.drawable.above_shadow));
-        layout.setAnchorPoint(0.3f);
-        layout.setPanelSlideListener(new PanelSlideListener() {
-            @Override
-            public void onPanelSlide(View panel, float slideOffset) {
-                Log.i(TAG, "onPanelSlide, offset " + slideOffset);
-                if (slideOffset < 0.2) {
-                    if (getActionBar().isShowing()) {
-                        getActionBar().hide();
-                    }
-                } else {
-                    if (!getActionBar().isShowing()) {
-                        getActionBar().show();
-                    }
-                }
-            }
+		View dragme = findViewById(R.id.dragme);
 
-            @Override
-            public void onPanelExpanded(View panel) {
-                Log.i(TAG, "onPanelExpanded");
+		SlidingUpPanelLayout layout = (SlidingUpPanelLayout) findViewById(R.id.sliding_layout);
+		layout.setShadowDrawable(getResources().getDrawable(R.drawable.above_shadow));
+		layout.setAnchorPoint(0.3f);
+		layout.setDragView(dragme);
+		layout.setCoveredFadeColor(Color.TRANSPARENT);
+		layout.setPanelSlideListener(new PanelSlideListener() {
+			@Override
+			public void onPanelSlide(View panel, float slideOffset) {
+				Log.i(TAG, "onPanelSlide, offset " + slideOffset);
+			}
 
-            }
+			@Override
+			public void onPanelExpanded(View panel) {
+				Log.i(TAG, "onPanelExpanded");
 
-            @Override
-            public void onPanelCollapsed(View panel) {
-                Log.i(TAG, "onPanelCollapsed");
+			}
 
-            }
+			@Override
+			public void onPanelCollapsed(View panel) {
+				Log.i(TAG, "onPanelCollapsed");
 
-            @Override
-            public void onPanelAnchored(View panel) {
-                Log.i(TAG, "onPanelAnchored");
+			}
 
-            }
-        });
+			@Override
+			public void onPanelAnchored(View panel) {
+				Log.i(TAG, "onPanelAnchored");
 
-        TextView t = (TextView) findViewById(R.id.main);
-        t.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(Intent.ACTION_VIEW);
-                i.setData(Uri.parse("http://www.umanoapp.com"));
-                startActivity(i);
-            }
-        });
+			}
+		});
 
-        t = (TextView) findViewById(R.id.name);
-        t.setText(Html.fromHtml(getString(R.string.hello)));
-        t = (TextView) findViewById(R.id.follow);
-        t.setText(Html.fromHtml(getString(R.string.follow)));
-        t.setMovementMethod(LinkMovementMethod.getInstance());
+		ListView list = (ListView) findViewById(R.id.main);
+		ListAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, Cheeses.sCheeseStrings);
+		list.setAdapter(adapter);
+		list.setOnItemClickListener(new OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+				String s = (String) arg0.getAdapter().getItem(arg2);
+				Toast.makeText(arg0.getContext(), s, Toast.LENGTH_SHORT).show();
+			}
+		});
 
-        boolean actionBarHidden = savedInstanceState != null ?
-                savedInstanceState.getBoolean(SAVED_STATE_ACTION_BAR_HIDDEN, false): false;
-        if (actionBarHidden) {
-            getActionBar().hide();
-        }
-    }
-
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putBoolean(SAVED_STATE_ACTION_BAR_HIDDEN, !getActionBar().isShowing());
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.demo, menu);
-        return true;
-    }
+		list = (ListView) findViewById(R.id.list2);
+		List<String> l = Arrays.asList(Cheeses.sCheeseStrings);
+		Collections.reverse(l);
+		adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, l);
+		list.setAdapter(adapter);
+		list.setOnItemClickListener(new OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+				String s = (String) arg0.getAdapter().getItem(arg2);
+				Toast.makeText(arg0.getContext(), s, Toast.LENGTH_SHORT).show();
+			}
+		});
+	}
 
 }
